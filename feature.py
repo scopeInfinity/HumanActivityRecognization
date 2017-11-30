8a2b0829d77614576a86a74945d8bf4b6873d981
scopeinfinity <gagan1kumar@ymail.com>
2017-11-30 23:31:08 +0530

    Some tweeks

--- feature.py
+++ feature.py
@@ -3,24 +3,8 @@ import cv2
 import os
 
 def encodeFeature(in_):
+	# If we want to reduce dimension or similar
 	return in_
-	l = len(in_)
-	signal_level = 8
-	nw = []
-	for w in in_:
-		lev = 0
-		for j in range(signal_level):
-			if 1.0*(j+1)/signal_level <= w:
-				lev = j+1
-	nw.append(lev)
-	p = 1.0
-	val = 0.0
-	for w in nw:
-		w = w * p
-		val += w
-		p = p*signal_level
-	val = val/p
-	return val
 
 def getTime(frames,tsecs,_id):
 	time = _id*tsecs/frames
@@ -31,38 +15,29 @@ def getTime(frames,tsecs,_id):
 	time = "%0d:%02d:%02d.%s" % (h,m,s,ms)
 	return time
 
-def getFrame(fname,_id,tsecs,frames,cc):
-	time = getTime(frames,tsecs,_id)
-
-	print "Frame %d at %s" % (_id,time)
-	os.system("ffmpeg -y -i %s -vcodec png -ss %s -vframes 1 -an -f rawvideo temp/%d.png"%(fname,time,cc))
-
-
 def getFrameCount(fname):
 	return int(os.popen("ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1  %s"%fname).read())
 
 def load_video(fname,startf,endframe):
 	duration=os.popen("ffmpeg -i %s 2>&1 | grep Duration | awk '{print $2}' | tr -d ,"%fname).read().split(":")
 	tsecs = 0
-	print fname
-	print duration
+	print "Loading video %s " % fname
+	# print duration
 	tsecs += int(duration[0])*3600
 	tsecs += int(duration[1])*60
 	tsecs += float(duration[2])
-	print tsecs
+	# print tsecs
 	frames = getFrameCount(fname)
-	print frames
+	# print frames
 	os.popen("rm -r temp")
 	os.popen("mkdir temp")
 	stime = getTime(frames,tsecs, startf)
 	etime = getTime(frames,tsecs, endframe)
 	os.system(("ffmpeg -i %s -vsync vfr temp/"%fname+"%d.jpg -hide_banner"))
-	# os.system("ffmpeg -y -i %s -vcodec png -ss %s -vframes 1 -an -f rawvideo temp/%d.png"%(fname,time,cc))
-	print "all extracted"
+	
+	print "All frames extracted! %s " % fname
 	os.system("sleep 5s")
-	# for i,f in enumerate(range(startf,endframe)):
-	# 	getFrame(fname,f,tsecs,frames,i+startf)
-
+	
 def bestchoice(a,b):
 	# minx,miny,maxx,maxy
 	return (min(a[0],b[0]),min(a[01],b[1]),max(a[2],b[2]),max(a[3],b[3]))
@@ -94,16 +69,10 @@ def getFeatures(fname,startf,endframe):
 		try:
 			i= i2+startf
 			fn = 'temp/%d.jpg'%i
-			print "loading image %s" % fn
+			# print "loading image %s" % fn
 			img = cv2.imread(fn)
 			img = cv2.GaussianBlur(img, (5, 5), 0)
-			#if fFrame is None:
-			#		fFrame = frame
-			#	continue
 
-			#frameDelta = cv2.absdiff(fFrame, frame)
-			# print frameDelta
-			# exit()
 			frame = img
 			fgmask = fgbg.apply(img)
 			thresh = cv2.threshold(fgmask, 25, 255, cv2.THRESH_BINARY)[1]
@@ -120,15 +89,13 @@ def getFeatures(fname,startf,endframe):
 				continue
 			my /= conter
 			mx /= conter
-			if conter>0.8*len(thresh[0])*len(thresh):
+			if conter>0.5*len(thresh[0])*len(thresh):
 				continue
-			#print len(thresh),len(thresh[0])
-			#print my,mx
-			#print thresh[my][mx]
+
 			(minx,miny,maxx,maxy) = dfs(thresh.copy(),int(my),int(mx))
 			if minx == INF or miny == INF or maxx == -INF or maxy == -INF:
 				continue
-			#print (minx,miny,maxx,maxy)
+
 			newimg = thresh[miny:maxy, minx:maxx]
 			height = maxy-miny
 			width = maxx-minx
@@ -152,32 +119,15 @@ def getFeatures(fname,startf,endframe):
 			ox/=255.0
 			oy/=255.0
 
-			# print ox
-			# print oy
 
 			motionFeature = np.append(ox,oy)
-			print "Motion Feature"
-			print motionFeature
+			# print "Motion Feature"
+			# print motionFeature
 			encoded = encodeFeature(motionFeature)
 			clipFeature.append(motionFeature)
 
 
-			# Partioning into K blocks along x and y axis
-			# cv2.imshow('frame',newimg)
-			# k = cv2.waitKey(30) & 0xff
-			# if k == 27:
-			# 	break
 		except Exception as e:
 			print(e)
-	# cv2.destroyAllWindows()
 	return clipFeature
 
-
-# start = 215
-# end = 338
-# fname ="/home/scopeinfinity/HAR/videos/person15_walking_d1_uncomp.avi"
-# load_video(fname,start,end)
-
-# #data set
-# # http://www.nada.kth.se/cvap/actions/
-# getFeatures(fname,start,end)
--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from hmmlearn import hmm
 import pickle
 from sklearn.externals import joblib
 import numpy as np
-allowedclass = ["running","handclapping"]#,"jogging","boxing","handwaving","walking"]
+import glob,random
+import sys
+import argparse
+
+allowedclass = ["running","handclapping","jogging","walking"]#,"boxing","handwaving"
+
+# #data set
+# # http://www.nada.kth.se/cvap/actions/
 
 def train():
-	print "Started"
+	print "Train Started"
 	with open("00sequences.txt",'r') as f:
 		lines = f.readlines()
+
 	fclass = dict()
 	clength = dict()
 	model = dict()
@@ -17,37 +25,29 @@ def train():
 		fclass[c]= []
 		clength[c] = []
 		countdata[c] = 0
-		model[c] = hmm.GaussianHMM(2, "full")
-		# x1 = np.asarray([[0.1,0.2],[0.3,0.6],[0.5,0.9]],dtype=np.float32)
-		# x2 = np.asarray([[0.23,0.2],[0.23,0.6],[0.53,0.39]],dtype=np.float32)
-		# inn = np.concatenate([x1,x2])
-		# ll = np.array([len(x1),len(x2)])
-		# model[c].fit(inn, ll)
-		# exit()
+		model[c] = hmm.GaussianHMM(10, "full")
 
 	for line in lines:
 		tok =  line.split()
 		if len(tok)>0:
 			# print tok
 			fname = "videos/" + tok[0] + "_uncomp.avi"
-			print fname
 			c = tok[0].split("_")[1]
 			if c not in allowedclass:
 				continue
-			print c
+
+			print "Fname : %s \t Class : %s " % (fname,c)
+			if countdata[c]>=1:
+				continue
+			countdata[c] += 1
 			frames = tok[2:]
 			for f in frames:
-				if countdata[c]>1:
-					continue
-				countdata[c] += 1
 				sf_ = f.split("-")
-				# print sf_
 				sf = int(sf_[0])
 				if sf_[1][-1]==',':
 					sf_[1]=sf_[1][0:-1]
-				# print sf_[1]
 				ef = int(sf_[1])
-				print sf,ef
+				#print sf,ef
 				f = feature.getFeatures(fname,sf,ef)
 				clength[c].append(len(f))
 				print f
@@ -57,26 +57,42 @@ def train():
 		print fclass[c]
 		print clength[c]
 		model[c].fit(np.array(fclass[c]),np.array(clength[c]))
-		fn = "model_"+c+".pkl"
+		fn = "model/model_"+c+".pkl"
 		joblib.dump(model[c], fn)
-		# with open(fn,'w') as f:
-		# 	pickle.dump(model[c],f)
+
+
+test_log_total   = 0
+test_log_correct = 0
+glob_msg = []
+
+def addMessage(s):
+	glob_msg.append(s)
+	print s
 
 def test(name):
+	global test_log_total
+	global test_log_correct
 	print "Started Test"
 	model = dict()
 	prob = dict()
 	for c in allowedclass:
-		fn = "model_"+c+".pkl"
+		fn = "model/model_"+c+".pkl"
 		prob[c] = []
 		model[c] = joblib.load(fn)
-	fname = "videos/" + name + "_uncomp.avi"
-	totalframe = 100#feature.getFrameCount(fname)
+	fname = name# "videos/" + name + "_uncomp.avi"
+	print fname
+	totalframe = feature.getFrameCount(fname)
 	W = 10
-
+	if totalframe<W:
+		if totalframe<2:
+			print "Too less features frame, ignoring..."
+			return
+		else:
+			W=totalframe
 	f = feature.getFeatures(fname,0,totalframe)
 	totalframe = len(f)
-	print ">>>> %d" % len(f)
+	
+	print "Feature Frames %d" % len(f)
 	for c in allowedclass:
 		print "Test for '%s'" % c
 		for i in range(0,totalframe/W):
@@ -86,8 +102,41 @@ def test(name):
 			print len(subfeature)
 			p = model[c].score(np.array(subfeature))
 			prob[c].append(p)
-	print prob
+	probClass = dict()
+	bClass = "none"
+	bProb = -float('inf')
+	for c in allowedclass:
+		probClass[c] = np.sum(prob[c])
+		if bProb<probClass[c]:
+			bProb=probClass[c]
+			bClass = c
+	aclass = name.split("_")[1]
+	addMessage("File : %s\tPredicted : [%s class] \tActual : [%s class]" % (name,bClass,aclass))
+	addMessage(str(probClass))
+	test_log_total  +=1
+	if aclass == bClass:
+		test_log_correct+=1
+
 
 if __name__ == '__main__':
-	#train()
-	test('person16_handclapping_d4')
+	parser = argparse.ArgumentParser()
+	parser.add_argument("mode",choices=["train","test"])
+	args = parser.parse_args()
+	if args.mode == "train":
+		train()
+	else:
+		fnames = []
+		testCountPerClass = 1
+		for c in allowedclass:
+			X=glob.glob("videos/person*_%s_*"%c)
+			random.shuffle(X)
+			fnames.extend(X[:testCountPerClass])
+		print "Testing for ...."
+		print fnames
+		for fn in fnames:
+			test(fn)
+		for l in glob_msg:
+			print l
+		print ""
+		print "*" * 20
+		print "Test Accuracy : %2.2f " % (test_log_correct*100.0/test_log_total)
